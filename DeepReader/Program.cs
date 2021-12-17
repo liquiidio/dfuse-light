08c38d5848ed83db54e309b2e5cc26a2c27f2ed1
cmadh_ <grizzlycorv@gmail.com>
2021-12-17 22:09:58 +0100

    initial commit

---
+++ DeepReader/Program.cs
@@ -0,0 +1,9 @@
+using DeepReader;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => { services.AddHostedService<Worker>(); })
+    .Build();
+
+await host.RunAsync();
