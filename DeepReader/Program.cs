d26e560f0b8c5063fe81aeb90c8ce9d3194d7a18
cmadh_ <grizzlycorv@gmail.com>
2021-12-24 22:45:21 +0100

    process-start and logging

--- DeepReader/Program.cs
+++ DeepReader/Program.cs
@@ -7,6 +7,8 @@ using System.Threading.Channels;
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services => {
         services.AddSingleton(Channel.CreateUnbounded<Block>(new UnboundedChannelOptions() { SingleReader = false, SingleWriter = true }));
+        services.AddSingleton(svc => svc.GetRequiredService<Channel<Block>>().Reader);
+        services.AddSingleton(svc => svc.GetRequiredService<Channel<Block>>().Writer);
         services.AddHostedService<Worker>();
         services.AddHostedService<BlockWorker>();
     })
--- DeepReader/Worker.cs
+++ DeepReader/Worker.cs
@@ -24,42 +24,108 @@ namespace DeepReader
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
-            }
+            await StartNodeos(stoppingToken);
         }
-        
+
         private async Task StartNodeos(CancellationToken stoppingToken)
         {
             // TODO check nodeos version, provide arguments-list
 
-            using var process = Process.Start(
-                new ProcessStartInfo
+//nodeos -e -p eosio \
+//--delete-all-blocks \
+//--deep-mind \
+//--plugin eosio::producer_plugin \
+//--plugin eosio::producer_api_plugin \
+//--plugin eosio::chain_api_plugin \
+//--plugin eosio::http_plugin \
+//--plugin eosio::history_plugin \
+//--plugin eosio::history_api_plugin \
+//--filter-on="*" \
+//--access-control-allow-origin='*' \
+//--contracts-console \
+//--http-validate-host=false \
+//--verbose-http-errors >> nodeos.log 2 > &1 &
+
+            //var args = new System.Collections.ObjectModel.Collection<string>()
+            //{
+            //    "-e", "-p eosio",
+            //    "--delete-all-blocks",
+            //    "--deep-mind",
+            //    "--plugin eosio::producer_plugin",
+            //    "--plugin eosio::producer_api_plugin",
+            //    "--plugin eosio::chain_api_plugin",
+            //    "--plugin eosio::http_plugin",
+            //    "--plugin eosio::history_plugin",
+            //    "--plugin eosio::history_api_plugin",
+            //    "--filter-on = \"*\"",
+            //    "--access-control-allow-origin = '*'",
+            //    "--contracts-console",
+            //    "--http-validate-host = false",
+            //    "--verbose-http-errors >> nodeos.log 2 > &1 &"
+            //};
+
+            //ArgumentList =
+            //    {
+            //    "-e", "-p eosio",
+            //        "--delete-all-blocks",
+            //        "--deep-mind",
+            //        "--plugin eosio::producer_plugin",
+            //        "--plugin eosio::producer_api_plugin",
+            //        "--plugin eosio::chain_api_plugin",
+            //        "--plugin eosio::http_plugin",
+            //        "--plugin eosio::history_plugin",
+            //        "--plugin eosio::history_api_plugin",
+            //        "--filter-on = \"*\"",
+            //        "--access-control-allow-origin = '*'",
+            //        "--contracts-console",
+            //        "--http-validate-host = false",
+            //        "--verbose-http-errors >> nodeos.log 2 > &1 &"
+            //    }, // { "--delete-all-blocks --deep-mind" },
+
+
+            using var process = new Process();
+            process.StartInfo = new ProcessStartInfo
+            {
+                FileName = "nodeos",
+                ArgumentList =
                 {
-                    // TOOD
-                    FileName = "nodeos",
-                    ArgumentList = { "", },
-                    UseShellExecute = false,
-                    RedirectStandardError = true,
-                    RedirectStandardInput = true,
-                    RedirectStandardOutput = true,
-                    CreateNoWindow = true,
-                });
+                    "-e" ,"-p", "eosio", "--delete-all-blocks", "--deep-mind"
+//                    "-e -p eosio --delete-all-blocks --deep-mind --plugin eosio::producer_plugin --plugin eosio::producer_api_plugin --plugin eosio::chain_api_plugin --plugin eosio::http_plugin --plugin eosio::history_plugin --plugin eosio::history_api_plugin --filter-on='*' --access-control-allow-origin='*' --contracts-console --http-validate-host=false --verbose-http-errors"// >> nodeos.log 2 > &1 &
+                }, // { "--delete-all-blocks --deep-mind" },
+                UseShellExecute = false,
+                RedirectStandardError = true,
+                RedirectStandardInput = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true,
+                WorkingDirectory = "/usr/bin",
+            };
 
             if (process != null)
             {
-                process.OutputDataReceived += async (sender, e) => await OnNodeosOutputDataReceived(sender, e, stoppingToken);
+                process.OutputDataReceived += async (sender, e) => await OnNodeosDataReceived(sender, e, stoppingToken); ;// async (sender, e) => await OnNodeosOutputDataReceived(sender, e, stoppingToken);
+                process.ErrorDataReceived += async (sender, e) => await OnNodeosDataReceived(sender, e, stoppingToken);
+                process.Exited += OnNodeosExited;
 
                 process.Start();
+                process.BeginErrorReadLine();
                 process.BeginOutputReadLine();
-
-                await process.WaitForExitAsync(stoppingToken);
+                //await process.WaitForExitAsync(stoppingToken);
+                while (!process.HasExited)
+                {
+                    await Task.Delay(10000);
+                    // do nothing
+                    string test = "";
+                };
+                Console.WriteLine("EXITED 1");
             }
         }
-        
-        private async Task OnNodeosOutputDataReceived(object sender, DataReceivedEventArgs e, CancellationToken ctl)
+
+        private void OnNodeosExited(object? sender, EventArgs e)
+        {
+            Console.WriteLine($"Nodeos exited2");
+        }
+
+        private async Task OnNodeosDataReceived(object sender, DataReceivedEventArgs e, CancellationToken clt)
         {
             if (e.Data != null)
             {
@@ -67,58 +133,58 @@ namespace DeepReader
                 {
                     var ctx = new ParseCtx();
 
-					var data = e.Data.Split(' ');
+                    var data = e.Data.Split(' ');
 
                     switch (data[1])
                     {
-						case "RAM_OP":
+                        case "RAM_OP":
                             ctx.ReadRamOp(data[Range.StartAt(2)]);
-							break;
+                            break;
                         case "CREATION_OP":
                             ctx.ReadCreationOp(data[Range.StartAt(2)]);
                             break;
                         case "DB_OP":
                             ctx.ReadDbOp(data[Range.StartAt(2)]);
-							break;
+                            break;
                         case "RLIMIT_OP":
                             ctx.ReadRlimitOp(data[Range.StartAt(2)]);
-							break;
+                            break;
                         case "TRX_OP":
                             ctx.ReadTrxOp(data[Range.StartAt(2)]);
-							break;
+                            break;
                         case "APPLIED_TRANSACTION":
                             ctx.ReadAppliedTransaction(data[Range.StartAt(2)]);
-							break;
+                            break;
                         case "TBL_OP":
                             ctx.ReadTableOp(data[Range.StartAt(2)]);
-							break;
+                            break;
                         case "PERM_OP":
                             ctx.ReadPermOp(data[Range.StartAt(2)]);
-							break;
+                            break;
                         case "DTRX_OP CREATE":
                             ctx.ReadCreateOrCancelDTrxOp("CREATE", data[Range.StartAt(2)]);
-							break;
+                            break;
                         case "DTRX_OP MODIFY_CREATE":
                             ctx.ReadCreateOrCancelDTrxOp("MODIFY_CREATE", data[Range.StartAt(2)]);
-							break;
+                            break;
                         case "DTRX_OP MODIFY_CANCEL":
                             ctx.ReadCreateOrCancelDTrxOp("MODIFY_CANCEL", data[Range.StartAt(2)]);
-							break;
+                            break;
                         case "RAM_CORRECTION_OP":
                             ctx.ReadRamCorrectionOp(data[Range.StartAt(2)]);
-							break;
+                            break;
                         case "DTRX_OP PUSH_CREATE":
                             ctx.ReadCreateOrCancelDTrxOp("PUSH_CREATE", data[Range.StartAt(2)]);
-							break;
+                            break;
                         case "DTRX_OP CANCEL":
                             ctx.ReadCreateOrCancelDTrxOp("CANCEL", data[Range.StartAt(2)]);
-							break;
+                            break;
                         case "DTRX_OP FAILED":
                             ctx.ReadFailedDTrxOp(data[Range.StartAt(2)]);
-							break;
+                            break;
                         case "ACCEPTED_BLOCK":
-							var block = ctx.ReadAcceptedBlock(data[Range.StartAt(2)]);
-                            await _blocksChannel.WriteAsync(block, ctl);
+                            var block = ctx.ReadAcceptedBlock(data[Range.StartAt(2)]);
+                            await _blocksChannel.WriteAsync(block, clt);
                             /*if err != nil {
 							    return null;, l.formatError(line, err);
                             }*/
@@ -126,35 +192,40 @@ namespace DeepReader
                             break;
                         case "START_BLOCK":
                             ctx.ReadStartBlock(data[Range.StartAt(2)]);
-							break;
+                            break;
                         case "FEATURE_OP ACTIVATE":
                             ctx.ReadFeatureOpActivate(data[Range.StartAt(2)]);
-							break;
+                            break;
                         case "FEATURE_OP PRE_ACTIVATE":
                             ctx.ReadFeatureOpPreActivate(data[Range.StartAt(2)]);
-							break;
+                            break;
                         case "SWITCH_FORK":
                             //zlog.Info("fork signal, restarting state accumulation from beginning");
                             ctx.ResetBlock();
-							break;
+                            break;
                         case "ABIDUMP START":
                             ctx.ReadAbiStart(data[Range.StartAt(2)]);
-							break;
+                            break;
                         case "ABIDUMP ABI":
                             ctx.ReadAbiDump(data[Range.StartAt(2)]);
-							break;
+                            break;
                         case "ABIDUMP END":
-						//noop
-    						break;
-						case "DEEP_MIND_VERSION":
+                            //noop
+                            break;
+                        case "DEEP_MIND_VERSION":
                             ctx.ReadDeepmindVersion(data[Range.StartAt(2)]);
-							break;
+                            break;
                         default:
+                            Console.WriteLine(e.Data);
                             break;
                             //zlog.Info("unknown log line", zap.String("line", data));
                     }
-				}
+                }
+                else
+                    Console.WriteLine($"{e.Data}");
             }
+            else
+                Console.WriteLine("data is null");
         }
     }
 }
