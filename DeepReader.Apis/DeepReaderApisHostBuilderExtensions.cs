bbf2a117b6c917d11f93471ebaa820e9a2fc6b29
Haron Kipkorir <ronoharonkip@gmail.com>
2022-04-26 16:00:50 +0300

    changed ihostbuilder configuration for deepreader.apis project

--- DeepReader.Apis/DeepReaderApisHostBuilderExtensions.cs
+++ DeepReader.Apis/DeepReaderApisHostBuilderExtensions.cs
@@ -8,24 +8,14 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Prometheus;
 
-namespace DeepReader.Apis.REST
+namespace DeepReader.Apis
 {
-    /// <summary>
-    /// Extends <see cref="T:Microsoft.Extensions.Hosting.IHostBuilder" /> with Serilog configuration methods.
-    /// </summary>
-    public static class DeepReaderRestHostBuilderExtensions
+    public static class DeepReaderApisHostBuilderExtensions
     {
-        /// <summary>Sets Faster as the logging provider.</summary>
-        /// <param name="builder">The host builder to configure.</param>
-        /// <param name="logger">The Serilog logger; if not supplied, the static <see cref="T:Serilog.Log" /> will be used.</param>
-        /// <param name="dispose">When <c>true</c>, dispose <paramref name="logger" /> when the framework disposes the provider. If the
-        /// logger is not specified but <paramref name="dispose" /> is <c>true</c>, the <see cref="M:Serilog.Log.CloseAndFlush" /> method will be
-        /// called on the static <see cref="T:Serilog.Log" /> class instead.</param>
-        /// <returns>The host builder.</returns>
-        public static IHostBuilder UseDeepReaderRest(
-            this IHostBuilder builder,
-            ILogger logger = null,
-            bool dispose = false)
+        public static IHostBuilder UseDeepReaderApis(
+           this IHostBuilder builder,
+           ILogger logger = null,
+           bool dispose = false)
         {
             if (builder == null)
                 throw new ArgumentNullException(nameof(builder));
@@ -63,7 +53,7 @@ namespace DeepReader.Apis.REST
                     });
 
                     app.UseEndpoints(endpoints =>
-                    { 
+                    {
                         endpoints.MapControllers();
                         endpoints.MapMetrics();
                     });
