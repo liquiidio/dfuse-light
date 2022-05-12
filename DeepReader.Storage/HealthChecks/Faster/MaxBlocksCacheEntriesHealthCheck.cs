0031f64733dbcc85d3494e8fc77288e95bfdb6d5
Haron Kipkorir <ronoharonkip@gmail.com>
2022-05-12 23:58:11 +0300

    added some health checks for faster storage options

---
+++ DeepReader.Storage/HealthChecks/Faster/MaxBlocksCacheEntriesHealthCheck.cs
@@ -0,0 +1,21 @@
+﻿using DeepReader.Storage.Options;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+
+namespace DeepReader.Storage.HealthChecks.Faster
+{
+    public class MaxBlocksCacheEntriesHealthCheck : IHealthCheck
+    {
+        public IOptionsMonitor<FasterStorageOptions> StorageOptionsMonitor { get; }
+
+        public MaxBlocksCacheEntriesHealthCheck(IOptionsMonitor<FasterStorageOptions> storageOptionsMonitor)
+        {
+            StorageOptionsMonitor = storageOptionsMonitor;
+        }
+
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(HealthCheckResult.Healthy("MaxBlocksCacheEntries: " + StorageOptionsMonitor.CurrentValue.MaxBlocksCacheEntries));
+        }
+    }
+}
