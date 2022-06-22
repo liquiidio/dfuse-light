24cd83bcd240b708d9b219b2157a72618d559699
Haron Kipkorir <ronoharonkip@gmail.com>
2022-06-22 22:42:02 +0300

    Introduced IFasterSerializable interface that will be used to denote types that are going to saved to buffer, this also allows IEosioSerializable to implement BinaryBufferReader.

---
+++ DeepReader.Types/Interfaces/IFasterSerializable.cs
@@ -0,0 +1,8 @@
+﻿namespace DeepReader.Types.Interfaces
+{
+    public interface IFasterSerializable<T>
+    {
+        public T Deserialize(BinaryReader reader);
+        public void Serialize(BinaryWriter writer);
+    }
+}
